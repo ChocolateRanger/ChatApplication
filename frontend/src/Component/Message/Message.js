b4920c6b9c63c49c5b53dc6baf43a24797caf803
Aditya <Adiadi7898@gmail.com>
2023-01-19 16:34:42 +0530

    Added CSS

--- frontend/src/Component/Message/Message.js
+++ frontend/src/Component/Message/Message.js
@@ -27,31 +27,39 @@ const Message = ({ user, text, name }) => {
             <div className='box-right message-right'>
               {text}
             </div>
+            {/* <div className='timeStamp'>
+              Time
+            </div> */}
           </div>
 
           :
 
           (isAdmin) ?
-            <div>
+            <div className='messages'>
               <div className='messages-center'>
                 <div className='box-center'>
                   {text}
                 </div>
+                {/* <div className='timeStamp'>
+                  Time
+                </div> */}
               </div>
             </div>
 
             :
 
-            <div>
-              <div className='messages'>
-                <div className='message-user-left'>
-                  {user}
-                </div>
-                <div className='box-left message-left'>
-                  {text}
-                </div>
+            <div className='messages'>
+              <div className='message-user-left'>
+                {user}
               </div>
+              <div className='box-left message-left'>
+                {text}
+              </div>
+              {/* <div className='timeStamp'>
+                Time
+              </div> */}
             </div>
+
       }
 
 
